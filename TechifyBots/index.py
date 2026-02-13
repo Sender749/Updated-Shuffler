3deeeceb872e17461a4a61b212c0e1d2e7097873
Sender749 <jotmahal2003@gmail.com>
2026-02-13 15:10:47 +0530

    Update index.py

--- TechifyBots/index.py
+++ TechifyBots/index.py
@@ -8,10 +8,12 @@ from pyrogram.types import *
 async def save_video(client: Client, message: Message):
     try:
         video_id = message.id
+        file_id = message.video.file_id
         video_duration = message.video.duration
         is_premium = video_duration > FREE_VIDEO_DURATION
-        await mdb.save_video_id(video_id, video_duration, is_premium)
+        await mdb.save_video_id(video_id, file_id, video_duration, is_premium)
         text = f"**✅ Saved | ID: {video_id} | ⏱️ {video_duration}s | 💎 {is_premium}**"
         await client.send_message(chat_id=DATABASE_CHANNEL_LOG, text=text)
     except Exception as t:
         print(f"Error: {str(t)}")
+
